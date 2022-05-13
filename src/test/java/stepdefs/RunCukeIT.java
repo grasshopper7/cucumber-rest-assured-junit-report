0a5d77c26efda267a4d2123396031d63d7dbfadc
grasshopper7 <grass.hopper.moc@gmail.com>
2022-05-13 22:24:56 +0530

    Updated version

--- src/test/java/stepdefs/RunCukeIT.java
+++ src/test/java/stepdefs/RunCukeIT.java
@@ -6,9 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = { "src/test/resources/" },  plugin = { "summary",
-		"json:target/cucumber-json.json",
-		"tech.grasshopper.AllureCucumberMappingPlugin:target/cucumber-allure.json" }/* , tags = "@city" */)
+@CucumberOptions(features = { "src/test/resources/" }, plugin = { "summary", "json:target/cucumber-json.json",
+		"tech.grasshopper.AllureCucumberMappingPlugin:target/cucumber-allure.json" })
 public class RunCukeIT {
 
 }
